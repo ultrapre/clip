356fc5b4a8f14af84284529fbd72fd247e07cf6d
wsl98501 <32613953+wsl98501@users.noreply.github.com>
2020-01-16 21:25:31 +0900

    update

---
+++ 汉化入门/2019-11-08-1573201655/assets/1573201655-340180458fca8334e5c8e48e185b7db7.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>TGBUS中国综合游戏门户_电玩巴士_电视游戏_电子游戏_网络游戏_手机游戏_网页游戏</title>
+  <meta name="keywords" content="电玩巴士,tgbus,电视游戏,电子游戏,网络游戏,手机游戏,网页游戏"/>
+<meta name="description" content="电玩巴士，中国综合游戏门户站，一直致力于发展中国电玩产业和网络游戏事业，提供游戏免费下载，及时的游戏资讯，完整的游戏攻略，及时的游戏视频攻略，以及打造拥有数千万会员的社交新媒体平台。"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="google-site-verification" content="bBMrQQM0M-qCFajZPLwIqIiguxI6VZYC5kphQoPUMNU" />
+  <meta name="domain_verify" content="pmrgi33nmfuw4ir2ej2goytvomxgg33neiwcez3vnfsceorcmy2wmylbga4tazjumiydiodfgbrgky3gmi3wmyzxmjrginzvgazcelbcoruw2zktmf3gkir2ge2tenjtgi4daojrge4tk7i">
+  <meta name="tencent-site-verification" content="73b184f56ea3bf50cc18793ad12c58bc"/>
+  <!-- <link rel="shortcut icon" type="images/x-icon" href="/static/img/favicon.ico"> -->
+  <link rel="stylesheet" href="/static/css/pc-reset.css">
+  <link rel="stylesheet" href="https://xyoss.g.com.cn/xy-production/static/swiper4.2.2.min.css">
+  <!--iconfont图标-->
+  <link rel="stylesheet" href="//at.alicdn.com/t/font_767020_otmb8gqekch.css">
+  <!--AD:通用广告 sdk-->
+  <!-- <script src="http://dssp.donews.com/170214/5/g.js"></script> -->
+  <script src="https://g1.tagtic.cn/g.js"></script>
+  <!--页面css-->
+  <link href="/dist/css/notFound-css.min.css?timestamp=1573195199" rel="stylesheet" type="text/css"/>
+  <script>
+    // 不是从tgbus wap端跳转过来
+    // 移动设备访问pc端
+    var isMobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+    var isMatchPage = function(page) {
+      var pageUrl = page ? '/' + page : ''
+      return (
+        document.referrer.indexOf('m.tgbus.com' + pageUrl) === -1 &&
+        document.referrer.indexOf('www.tgbus.com' + pageUrl) === -1 &&
+        (page ? window.location.href.indexOf('tgbus.com' + pageUrl) > 0 : true) &&
+        isMobile
+      )
+    }
+    var isChannel = function(url) {
+      return window.location.href.indexOf(url) > 0
+    }
+    if ((isMatchPage('news/') && !isChannel('m.tgbus.com'))  || (isMatchPage('live') && !isChannel('m.tgbus.com'))) {
+      window.location.href = '//m.tgbus.com' + window.location.pathname
+    } else if ((isMatchPage() && isChannel('www.tgbus.com/search')) || (isMatchPage() && isChannel('tech.tgbus.com/search')) || (isMatchPage() && isChannel('android.tgbus.com/search')) || (isMatchPage() && isChannel('iphone.tgbus.com/search'))) {
+      window.location.href = '//m.tgbus.com' + window.location.pathname
+    } else if (isMatchPage() && isChannel('tgbus.com/about/contact')) {
+      window.location.href = '//m.tgbus.com/contact'
+    } else if (isMatchPage() && isChannel('ps4.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/list/ps4'
+    } else if (isMatchPage() && isChannel('xbox.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/list/xbox'
+    } else if (isMatchPage() && isChannel('ns.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/list/ns'
+    } else if (isMatchPage() && isChannel('tech.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/list/tech'
+    } else if (isMatchPage() && isChannel('android.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/list/android'
+    } else if (isMatchPage() && isChannel('iphone.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/list/iphone'
+    } else if (isMatchPage() && isChannel('e3.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/e3'
+    } else if (isMatchPage() && isChannel('e3.a9vg.com')) {
+      window.location.href = '//m.a9vg.com/e3'
+    } else if (isMatchPage() && isChannel('tgs.tgbus.com')) {
+      window.location.href = '//m.tgbus.com/tgs'
+    } else if (isMatchPage() && isChannel('tgs.a9vg.com')) {
+      window.location.href = '//m.a9vg.com/tgs'
+    } else if (isMatchPage()) {
+      window.location.href = '//m.tgbus.com'
+    }
+    // 判断是否为a9vg e3专题
+    // open
+    var link = document.createElement('link')
+    link.rel = 'shortcut icon'
+    link.type = 'images/x-icon'
+    if (window.location.href.indexOf('a9vg.com') > 0) {
+      link.href = '/static/img/favicon-a9vg.ico'
+    } else {
+      link.href = '/static/img/favicon.ico'
+    }
+    var l = document.getElementsByTagName('link')[0]
+    l.parentNode.insertBefore(link, l)
+    // end
+  </script>
+</head>
+<body data-site="tgbus">
+<div id="app" data-server-rendered="true"><div class="nf"><div class="tb-header"><div class="tb-header__logo-box"><a href="https://www.tgbus.com/" target="_blank" title="电玩巴士" class="tb-header__logo"><img src="/static/img/logo.png" alt></a></div><div class="tb-header__border-line"></div><div class="tb-header__center-line"></div><div class="tb-header__nav-box"><ul class="tb-header__nav"><li class="tb-header__nav-item"><div target="_blank" href="javascript:void(0)" class="tb-header__nav-link">电玩
+        </div><div class="tb-header__nav-content" style="display:none;"><a target="_blank" href="https://ps4.tgbus.com/"><img src="/static/img/header/dw-ps4.png"><p>PS4</p></a><a target="_blank" href="https://switch.tgbus.com/"><img src="/static/img/header/dw-switch.png"><p>Switch</p></a><a target="_blank" href="https://xboxone.tgbus.com/ "><img src="/static/img/header/dw-xboxone.png"><p>XBOX ONE</p></a><a target="_blank" href="http://psv.tgbus.com/"><img src="/static/img/header/dw-psv.png"><p>PSV</p></a><a target="_blank" href="http://3ds.tgbus.com/"><img src="/static/img/header/dw-3ds.png"><p>3DS</p></a><a target="_blank" href="http://psp.tgbus.com/"><img src="/static/img/header/dw-psp.png"><p>PSP</p></a><a target="_blank" href="http://nds.tgbus.com/"><img src="/static/img/header/dw-nds.png"><p>NDS</p></a><a target="_blank" href="http://ps3.tgbus.com/"><img src="/static/img/header/dw-ps3.png"><p>PS3</p></a><a target="_blank" href="http://xbox360.tgbus.com/"><img src="/static/img/header/dw-xbox360.png"><p>XBOX360</p></a></div></li><li class="tb-header__nav-item"><div target="_blank" href="javascript:void(0)" class="tb-header__nav-link">游戏
+        </div><div class="tb-header__nav-content" style="display:none;"><a target="_blank" href="https://game.tgbus.com/"><img src="/static/img/header/yx-yxk.png"><p>游戏库</p></a><a target="_blank" href="http://pc.tgbus.com/"><img src="/static/img/header/yx-dj.png"><p>单机</p></a><a target="_blank" href="http://ol.tgbus.com/"><img src="/static/img/header/yx-wy.png"><p>网游</p></a><a target="_blank" href="http://shouji.tgbus.com/"><img src="/static/img/header/yx-sy.png"><p>手游</p></a></div></li><li class="tb-header__nav-item"><div target="_blank" href="javascript:void(0)" class="tb-header__nav-link">数码
+        </div><div class="tb-header__nav-content" style="display:none;"><a target="_blank" href="http://tech.tgbus.com/"><img src="/static/img/header/sm-hi.png"><p>Hi科技</p></a><a target="_blank" href="http://iphone.tgbus.com/"><img src="/static/img/header/sm-iphone.png"><p>iPhone中文网</p></a><a target="_blank" href="http://android.tgbus.com/"><img src="/static/img/header/sm-android.png"><p>安卓中文网</p></a><a target="_blank" href="http://www.tgbus.com/live"><img src="/static/img/header/sm-live.png"><p>巴士直播站</p></a></div></li><li class="tb-header__nav-item"><a target="_blank" rel="nofollow" href="http://www.tgbus.com/news/34584/" class="tb-header__nav-link">直播
+        </a><!----></li><li class="tb-header__nav-item"><a target="_blank" rel="nofollow" href="https://tg-bus.taobao.com/" class="tb-header__nav-link">商城
+        </a><!----></li><li class="tb-header__nav-item"><a target="_blank" rel="nofollow" href="http://www.tgbus.com/about/contact" class="tb-header__nav-link">加盟
+        </a><!----></li></ul></div><div class="tb-header__search-box"><input type="text"><img src="/static/img/home/ic_search.png"></div><div class="tb-header__login"><span class="register">注册</span><span class="signup">登录</span></div><!----><!----></div><div class="nf__content"><img src="/static/img/404/404.png"><div class="nf__link"><p>你访问的页面被吃掉啦</p><a href="/">回到首页</a></div></div></div></div>
+</body>
+<script>window.__INITIAL_STATE__={"route":{"path":"/404","hash":"","query":{},"params":{},"fullPath":"/404","meta":{},"from":{"name":null,"path":"/","hash":"","query":{},"params":{},"fullPath":"/","meta":{}}}};(function(){var s;(s=document.currentScript||document.scripts[document.scripts.length-1]).parentNode.removeChild(s);}());</script>
+<script src="/dist/vendor.9af598dc3df32dfdbfee.js" defer></script><script src="/dist/notFound.9af598dc3df32dfdbfee.js" defer></script><script src="/dist/app.9af598dc3df32dfdbfee.js" defer></script>
+<script src="/static/js/swiper.js"></script>
+<script>
+  (function () {
+    // 没有被嵌入到iframe中
+    if (top !== self) {
+      var script = document.createElement('script')
+      script.src = 'https://xyoss.g.com.cn/xy-production/static/strategy.js'
+      script.charset = 'UTF-8'
+      // script.src = 'https://dreamwall.vue.gusaifei.com/strategy.js'
+      var s = document.getElementsByTagName('script')[0]
+      s.parentNode.insertBefore(script, s)
+    }
+  })()
+</script>
+<!-- BI统计代码 -->
+<script src="//bdtj.tagtic.cn/bi-sdk.1.2.1.js"></script>
+<script>
+  (function () {
+    window.BI_SDK.report('webtgbusweb', 'xy-log');
+  })()
+</script>
+<!-- 百度统计代码 -->
+<script type="text/javascript">
+  var _hmt = _hmt || [];
+  (function () {
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?ad6b0fd84f08dc70750c5ee6ba650172'
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(hm, s)
+
+    if(isChannel('iphone.tgbus.com')) {
+      hm = document.createElement('script')
+      hm.src = 'https://hm.baidu.com/hm.js?e9510b11f4be96624e87e58aa9021619'
+      s = document.getElementsByTagName('script')[0]
+      s.parentNode.insertBefore(hm, s)
+    }
+    if(isChannel('android.tgbus.com')) {
+      hm = document.createElement('script')
+      hm.src = 'https://hm.baidu.com/hm.js?805dc8a07eff052a35614357c2853559'
+      s = document.getElementsByTagName('script')[0]
+      s.parentNode.insertBefore(hm, s)
+    }
+    if(isChannel('tech.tgbus.com')) {
+      hm = document.createElement('script')
+      hm.src = 'https://hm.baidu.com/hm.js?ec876d8b20beb8c5254a8b816dc29e85'
+      s = document.getElementsByTagName('script')[0]
+      s.parentNode.insertBefore(hm, s)
+    }
+  })()
+</script>
+<!--AD:首页对联广告 sdk-->
+<!-- <script type="text/javascript" src="http://182.92.203.215:9001/v1/xingyou/c/440.js"></script> -->
+<!--AD:首页下拉广告 sdk-->
+<!-- <script type="text/javascript" src="http://182.92.203.215:9001/v1/xingyou/c/426.js"></script> -->
+</html>
